0c33a948f6242ec9dae22bf2a2bff53ed7ba16fb
eldertugboat786 <info@andreapadricelli.dev>
2020-02-11 19:21:30 +0100

    add stats endpoint skeleton

--- src/habits/status.js
+++ src/habits/status.js
@@ -2,4 +2,23 @@ const express = require('express');
 
 const router = express.Router();
 
-router.
+
+/*
+  se passo dateStart e dateEnd restituisco tutte le
+  statistiche per questo periodo
+*/
+router.get('/',(req,res){
+  res.json({
+
+  })
+})
+
+/*
+  se passo dateStart dateEnd prendo un periodo
+  per questo determinato id
+*/
+router.get('/:id', (req,res){
+  res.json({
+
+  })
+})
